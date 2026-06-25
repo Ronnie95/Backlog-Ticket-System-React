5e2514f4e9804b1fcf57abf04f2376ff65ae6d2e
Ronnie95 <t_daron@yahoo.com>
2026-06-25 15:38:44 -0400

    navbar added

--- backlog-frontend/src/App.jsx
+++ backlog-frontend/src/App.jsx
@@ -1,10 +1,17 @@
-
-  import React from "react";
-  import Home from "./Home";
+import React from "react";
+import Home from "./Home";
+import Navbar from "./components/NavBar"
   
   function App() {
     return (
+      <div>
+
       <Home />
+      <Navbar/>
+
+
+      </div>
+     
     );
   }
   
