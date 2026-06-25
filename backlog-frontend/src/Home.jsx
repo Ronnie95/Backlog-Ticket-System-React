8ce8a0fbf7d6c96511337c78a44bfdae4a96d94c
Ronnie95 <t_daron@yahoo.com>
2026-06-25 15:29:53 -0400

    bootstrap updates

--- backlog-frontend/src/Home.jsx
+++ backlog-frontend/src/Home.jsx
@@ -1,9 +1,23 @@
 
 
-function Home(){
-    return(
-        <h1>Hello</h1>
-    )
-}
+// function Home(){
+//     return(
+//         <h1>Hello</h1>
+//     )
+// }
 
-export default Home
+// export default Home
+
+function Home() {
+    return (
+      <div className="container mt-5">
+        <h1 className="text-primary">Bootstrap is Working!</h1>
+  
+        <button className="btn btn-success">
+          Click Me
+        </button>
+      </div>
+    );
+  }
+  
+  export default Home;
