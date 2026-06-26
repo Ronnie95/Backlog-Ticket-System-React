16ec3f2f7036f212070baa0016ffb283c7e0fc5e
Ronnie95 <t_daron@yahoo.com>
2026-06-26 01:04:00 -0400

    updates

---
+++ backlog-frontend/src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+function projects() {
+    const [projectForm, setProjectForm] = useState({
+
+        createdBy: "",
+        date: "",
+        projectName: "",
+        dueDate: "",
+        status: "",
+    })
+
+    const [projectInfo, setProjectInfo] = useState([])
+    useEffect(() =>
+    )
+}
